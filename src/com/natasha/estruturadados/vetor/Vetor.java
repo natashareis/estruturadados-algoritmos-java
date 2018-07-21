adef8c03f1dbaf7bf41b5a83e846b1086b423317
natashareis <visions.prelude@gmail.com>
2018-07-20 21:05:21 -0300

    Initial commit

---
+++ src/com/natasha/estruturadados/vetor/Vetor.java
@@ -0,0 +1,9 @@
+package com.natasha.estruturadados.vetor;
+
+public class Vetor {
+	private String[] elementos;
+	
+	public Vetor (int capacidade) {
+		this.elementos = new String[capacidade];
+	}
+}
---
+++ src/com/natasha/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,16 @@
+package com.natasha.estruturadados.vetor.teste;
+
+import com.natasha.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor (5);
+		
+		int i = 0;
+		i++;
+
+	}
+
+}
